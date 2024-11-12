8cf9956f024abe7c05b86ea435c82f3d43c06ee6
Diana <radudiaale27@gmail.com>
2024-11-12 09:09:46 +0100

    correcion

---
+++ Apuntes/Listas.java
@@ -0,0 +1,19 @@
+package Apuntes;
+
+public class Listas {
+    public static void main(String[] args) {
+    int [] diasSemanas = new int [8];
+
+    diasSemanas [0] = 10;
+    diasSemanas [1] = 9;
+    diasSemanas [2] = 6;
+    diasSemanas [3] = 20;
+    diasSemanas [4] = 15;
+    diasSemanas [5] = 0;
+    diasSemanas [6] = 3;
+
+    for(int i=0; i<diasSemanas.length; i++){
+        System.out.println(diasSemanas[i]);
+    }
+    }
+}
--- Relacion7/Ejercicio3.java
+++ Relacion7/Ejercicio3.java
@@ -4,12 +4,12 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Escribe cuantos arrays quieres:");
+        System.out.println("Escribe cuantos elementos de arrays quieres: ");
         int tamaño = entrada.nextInt();
         entrada.nextLine();
         String lista[] = new String[tamaño];
 
-        System.out.println("Escribe la array:");
+        System.out.println("Escribe el contenido de cada elemento: ");
         for (int i=0; i<tamaño; i++) {
             //lista[i] = entrada.next();
             lista[i] = entrada.nextLine();
@@ -18,13 +18,13 @@ public class Ejercicio3 {
             //Si ponemos otra entrada next line vacia esta memoria se limpia y no da error.
         }
 
-        System.out.println("Escribe un caracer: ");
+        System.out.println("Escribe un carácer: ");
         char caracter = entrada.next().charAt(0);
         
         for (int i=0; i<tamaño; i++){
             char letra = lista[i].charAt(0);
             if (letra == caracter) {
-                System.out.println(lista[i]);
+                System.out.println("los arrays que empiezan por" + caracter + "son: " + lista[i]);
             }
         }
         entrada.close();
